6f478690b5a25600339ede5a844417dcbd62b89d
Kopp <Kopp.Darin@epa.gov>
2025-06-02 14:49:47 -0700

    Fish MMI aquametBio calculations

--- R/2324_MMI_Calc.R
+++ R/2324_MMI_Calc.R
@@ -1,7 +1,7 @@
 # Calculate MMI
 # needed epa token to install... see email
 # devtools::install_github("USEPA/aquametBio")
-
+rm(list = ls())
 library(aquametBio)
 library(tidyverse)
 
@@ -17,30 +17,36 @@ dir_NRSA_2324 <- "O:/PRIV/CPHEA/PESD/COR/CORFILES/IM-TH007/data/im/nrsa2324"
 site.info <- dir_NRSA_2324 %>%
   paste0("/data/tabfiles/nrsa2324_siteinfo.tab") %>%
   read.table(sep = "\t", header = T)%>%
-  select("UID", "AG_ECO9")
+  select("UID", "SITE_ID", "AG_ECO9","WSAREASQKM")%>%
+  filter(!is.na(WSAREASQKM))
 
-# need watershed area from MW... 
+# 
+# # need watershed area from MW 5/28/2025... 
+# WSArea <- read.csv("data/NRSA2324_Watersheds.csv")%>%
+#   select("SITE_ID","WSAREASQKM")
+# 
+# site.info <- site.info %>% 
+#   left_join(WSArea,by = "SITE_ID") 
 
 
-#fish Count file -- Prepared by KB/NARS IM team-- merged with Ecosregions
-FishCnts <- dir_NRSA_2324%>%
+#fish Count file -- Prepared by KB/NARS IM team-- merged with Ecoregions
+FishCnts <- dir_NRSA_2324 %>%
   paste0("/data/tabfiles/nrsa2324_fishcount_data.tab") %>%
   read.table(sep = "\t", header = T) %>%
-  mutate(FINAL_CT = TOTAL)%>%
-  select("UID", "TAXA_ID", "FINAL_CT", "ANOM_CT", "IS_DISTINCT", "NON_NATIVE")%>%
-  left_join(site.info,by="UID")
+  mutate(FINAL_CT = TOTAL) %>%
+  select("UID", "TAXA_ID", "FINAL_CT", "ANOM_CT", "IS_DISTINCT", "NON_NATIVE","UNIQUE_ID")%>%
+  inner_join(site.info, by = "UID")
 
 #this should be the most recent taxa list
 FishTaxa <- dir_NRSA_2324 %>%
   paste0("/data/tabfiles/nrsa2324_fish_taxa.tab") %>%
   read.table(sep = "\t", header = T)
 
-
 #########################################
 
 # calculate fish MMI metrics
 ##########
-# This function calculates metrics for only the MMI for the ecoregion of each site.
+# This function calculates metrics for each ecoregion's MMI.
 outMets <- calcNRSA_FishMMImets(FishCnts, FishTaxa, "UID",
                                 ecoreg =  "AG_ECO9",
                                 dist = "IS_DISTINCT", ct = "FINAL_CT",
@@ -54,14 +60,19 @@ outMets <- calcNRSA_FishMMImets(FishCnts, FishTaxa, "UID",
 
 # calculate fish MMI
 ############
-# We have to supply the log10 of watershed area because some metrics in each MMI are adjusted for watershed size.
-#outMets.1 <- merge(outMets...)
-outMMI <- calcFishMMI(outMets.1, sampID = "UID", ecoreg = "ECO9", lwsarea = "LWSAREA")
+# We have to supply the log10 of watershed area because some metrics are adjusted for watershed size.
+outMets.1 <- merge(outMets[1:1267,], site.info[, c("UID", "SITE_ID", "WSAREASQKM")], by = "UID")%>%
+  mutate(LWSAREA = log10(WSAREASQKM), ECO9 = AG_ECO9)%>%
+  select(-c("AG_ECO9", "WSAREASQKM"))
+
+outMMI <- calcFishMMI(inMets = outMets.1, sampID = "UID", ecoreg = "ECO9", lwsarea = "LWSAREA")
 #########################################
 
 # assign condition
 ##########
 # Additional variables, including watershed area (km2) and total number of individuals 
 # collected would be required to assign condition only if we had missing MMI values.
-outCond <- ?assignFishCondition(outMMI, sampID = "UID", ecoreg = "ECO9", mmi = "MMI_FISH")
+outCond <- assignFishCondition(outMMI, sampID = "UID", ecoreg = "ECO9", mmi = "MMI_FISH")
 #########################################
+
+table(outCond$FISH_MMI_COND)
